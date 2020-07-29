d7d06e1a57b2ea5b344d1291db827fcca4a8ae85
James Arturo <jamesarturo1997@gmail.com>
2020-07-29 07:28:12 -0500

    cambio del cambio
    jajajajajaj cviva la vida

--- Sistema/DataTables/main.js
+++ Sistema/DataTables/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $('#TablaProductosSelecionados').DataTable({        
+    $('#TablaProductosSelecionados').DataTable({
         language: {
                 "lengthMenu": "Mostrar _MENU_ registros",
                 "zeroRecords": "No se encontraron resultados",
@@ -38,5 +38,93 @@ $(document).ready(function() {
 				className: 'btn btn-info'
 			},
 		]
+    });
+});
+
+// IDEA: datatable de ver cotizacion
+$(document).ready(function() {
+    $('#tablaCotizaciones').DataTable({
+        language: {
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "No se encontraron resultados",
+                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast":"Último",
+                    "sNext":"Siguiente",
+                    "sPrevious": "Anterior"
+          },
+          "sProcessing":"Procesando...",
+            },
+        //para usar los botones
+        responsive: "true",
+        dom: 'Bfrtilp',
+        buttons:[
+     {
+       extend:    'excelHtml5',
+       text:      '<i class="fas fa-file-excel"></i> ',
+       titleAttr: 'Exportar a Excel',
+       className: 'btn btn-success'
+     },
+     {
+       extend:    'pdfHtml5',
+       text:      '<i class="fas fa-file-pdf"></i> ',
+       titleAttr: 'Exportar a PDF',
+       className: 'btn btn-danger'
+     },
+     {
+       extend:    'print',
+       text:      '<i class="fa fa-print"></i> ',
+       titleAttr: 'Imprimir',
+       className: 'btn btn-info'
+     },
+   ]
+    });
+});
+// IDEA: tabla productos
+
+$(document).ready(function() {
+    $('#tablaProductos').DataTable({
+        language: {
+                "lengthMenu": "Mostrar _MENU_ registros",
+                "zeroRecords": "No se encontraron resultados",
+                "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sSearch": "Buscar:",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast":"Último",
+                    "sNext":"Siguiente",
+                    "sPrevious": "Anterior"
+          },
+          "sProcessing":"Procesando...",
+            },
+        //para usar los botones
+        responsive: "true",
+        dom: 'Bfrtilp',
+        buttons:[
+     {
+       extend:    'excelHtml5',
+       text:      '<i class="fas fa-file-excel"></i> ',
+       titleAttr: 'Exportar a Excel',
+       className: 'btn btn-success'
+     },
+     {
+       extend:    'pdfHtml5',
+       text:      '<i class="fas fa-file-pdf"></i> ',
+       titleAttr: 'Exportar a PDF',
+       className: 'btn btn-danger'
+     },
+     {
+       extend:    'print',
+       text:      '<i class="fa fa-print"></i> ',
+       titleAttr: 'Imprimir',
+       className: 'btn btn-info'
+     },
+   ]
     });
 });
