6447fdc8e3bd3c8a5ee3b504d274c4129a7c9773
CamiloDuarteC <65095031+CamiloDuarteC@users.noreply.github.com>
2020-08-04 08:32:00 -0500

    ffff

--- Sistema/js/validarR.js
+++ Sistema/js/validarR.js
@@ -1,50 +1,50 @@
 function validarr() {
-  var nombre, apellido, telefono, cc;
-  var genero, fechanan, correo, contraseña;
- nombre = document.getElementById("nombreRegistro").value;
- apellido = document.getElementById("apellido_registro").value;
- telefono = document.getElementById("telefono_registro").value;
- cc = document.getElementById("numeroidRegistro").value;
- genero = document.getElementById("genero_registro").value;
- fechanan = document.getElementById("fecha_registro").value;
- correo = document.getElementById("exampleInputEmail").value;
- contraseña = document.getElementById("exampleInputPassword").value;
+
+var nombre = document.getElementById("nombreRegistro").value;
+var apellido = document.getElementById("apellido_registro").value;
+var txtEdad2 = document.getElementById("telefono_registro").value;
+var cedula = document.getElementById("numeroidRegistro").value;
+var genero = document.getElementById("genero_registro").value;
+var fechanan = document.getElementById("fecha_registro").value;
+var txtCorreo = document.getElementById("exampleInputEmail").value;
+var contraseña = document.getElementById("exampleInputPassword").value;
 
 
-if (nombre === "") {
-  alert("el campo nombre esta basio");
+if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
+  alert("el campo nombre esta vacio");
   return false;
 
 }
 
-
-else if (apellido === "") {
- alert("el campo apellido esta basio");
+if (apellido == null || apellido.length == 0 || /^\s+$/.test(apellido)) {
+ alert("el campo apellido esta vacio");
 return false;
 }
 
-else if (telefono === "") {
- alert("el campo telefono apellido esta basio");
+if (!/^\d{9}$/ .test(txtEdad2)) {
+ alert("el campo telefono no es un numero");
 return false;
 }
-else if (cedula === "") {
- alert("el campo cedula esta basio");
+if (!/^\d{8}$/ .test(cedula)) {
+ alert("el campo cedula esta vacio");
 return false;
 }
-else if (genero === "") {
- alert("el campo genero esta basio");
+if (genero == null || genero.length == 0 || /^\s+$/.test(genero)) {
+ alert("el campo genero esta vacio");
 return false;
 }
-else if (fechanan === "") {
- alert("el campo fecha esta basio");
+if (!isNaN(fechanan)) {
+ alert("el campo fecha esta vacio");
 return false;
 }
-else if ( correo === "") {
- alert("el campo correo apellido esta basio");
+if (!(/\w+([-+.']\w+)*@\w+\.\S+/.test(txtCorreo))){
+ alert("el campo correo no es correcto");
 return false;
 }
-else if (contraseña === "") {
- alert("el campo contraseña esta basio");
+if (contraseña == null || contraseña.length == 0 || /^\s+$/.test(contraseña)) {
+ alert("el campo contraseña esta vacio");
 return false;
 }
+
+
 }
