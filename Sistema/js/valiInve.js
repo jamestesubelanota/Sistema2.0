41b158959cdb48c64247cd9447ee90ef5dfc54ef
CamiloDuarteC <65095031+CamiloDuarteC@users.noreply.github.com>
2020-08-05 23:05:31 -0500

    validacion

--- Sistema/js/valiInve.js
+++ Sistema/js/valiInve.js
@@ -1,11 +1,8 @@
 function validarFormulario3(){
  
     var txtNombre = document.getElementById('nombre').value;
-    var txtEdad = document.getElementById('numero').value;
     var txtEdad1 = document.getElementById('numero1').value;
     var txtEdad2 = document.getElementById('numero2').value;
-    var txtCorreo = document.getElementById('correo').value;
-    var txtFecha = document.getElementById('fecha').value;
     var cmbSelector = document.getElementById('selector').selectedIndex;
  
  
@@ -16,27 +13,10 @@ function validarFormulario3(){
     }
  
     //Test edad
-    if( !/^\d{7}$/ .test(txtEdad)){
-      alert('ERROR: Debe ingresar un numero');
-      return false;
-    }
     if(!/^\d{7}$/ .test(txtEdad1)){
         alert('ERROR: Debe ingresar un numero');
         return false;
       }
-      
- 
-    //Test correo
-    if(!(/\w+([-+.']\w+)*@\w+\.\S+/.test(txtCorreo))){
-      alert('ERROR: Debe escribir un correo válido');
-      return false;
-    }
- 
-    //Test fecha
-    if(!isNaN(txtFecha)){
-      alert('ERROR: Debe elegir una fecha');
-      return false;
-    }
  
     //Test comboBox
     if(cmbSelector == null || cmbSelector == 0){
